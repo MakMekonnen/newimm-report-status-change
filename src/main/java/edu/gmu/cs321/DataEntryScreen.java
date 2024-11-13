edc99806a9272b9164187f555b478a8d93468d24
Anon214 <anonrahman120@gmail.com>
2024-11-13 15:53:02 -0500

    changed fxml to border pane

--- src/main/java/edu/gmu/cs321/DataEntryScreen.java
+++ src/main/java/edu/gmu/cs321/DataEntryScreen.java
@@ -3,6 +3,7 @@ package edu.gmu.cs321;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,8 +16,9 @@ public class DataEntryScreen extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("DataEntryScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        BorderPane root = new BorderPane();
+
+        Scene scene = new Scene(root, 600, 400);
         stage.setTitle("Data Entry");
         stage.setScene(scene);
         stage.show();
